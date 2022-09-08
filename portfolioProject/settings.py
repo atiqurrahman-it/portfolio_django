6cba00ce2599d95aa551ee3cbed49f8ad4774a97
atiqur <atiqurrahman.cse2552@gmail.com>
2022-09-08 23:59:04 +0600

    media file update

--- portfolioProject/settings.py
+++ portfolioProject/settings.py
@@ -128,7 +128,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # py manage.py collectstatic
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
